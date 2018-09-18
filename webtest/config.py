21ac636552d1e13e46f396b3603d95644ff3e0ab
wilsenkosasih <aeroaeroga@yahoo.com>
2018-09-18 08:41:10 -0700

    test

--- webtest/config.py
+++ webtest/config.py
@@ -7,4 +7,4 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://gasme:8PE-dSn-MC9-MmW@gasmedb.c22idrbk0ds6.us-east-2.rds.amazonaws.com/dbgasme'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://gasme:8PE-dSn-MC9-MmW@gasmedb.c22idrbk0ds6.us-east-2.rds.amazonaws.com/gasmedb'
