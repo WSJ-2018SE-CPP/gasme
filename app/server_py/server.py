1bab1bf9e3487dc480409a640a8061f82ebee12e
xlei <xianmei_lei714@yahoo.com>
2018-10-20 14:01:47 -0700

    handling json file from python

--- app/server_py/server.py
+++ app/server_py/server.py
@@ -1,8 +1,49 @@
 from flask import Flask, jsonify, abort, request, make_response, url_for
 from flask_cors import CORS
+import json, os
 
 app = Flask(__name__, static_url_path = "")
 CORS(app)
+
+def get_json_file(path):
+    with open(path) as data_file:
+        return json.load(data_file)
+    return 0
+
+
+def decode_json(json_data):
+    if json_data:
+        #car
+        car = dict()
+        car["car_brand"] = json_data["car"]["car_brand"]
+        car["car_model"] = json_data["car"]["car_model"]
+        car["car_year"] = json_data["car"]["car_year"]
+        car["highway_consumption"] = json_data["car"]["highway_consumption"]
+        car["city_consumption"] = json_data["car"]["city_consumption"]
+        car["tank_capacity"] = json_data["car"]["tank_capacity"]
+        car["initial_gas_level"] = json_data["car"]["initial_gas_level"]
+        car["default"] = json_data["car"]["default"]
+        #gas
+        gas = dict()
+        gas["brand"] = json_data["gas"]["brand"]
+        #trip
+        trip = []
+        for one in json_data["trip"]:
+            trip.append({"long": one["long"], "lat": one["lat"]})
+        return car, gas, trip
+
+    return 0
+
+
+@app.route('/', methods = ['POST'])
+def from_jsonfile():
+    jdata = get_json_file(os.path.join(os.path.dirname(__file__), 'user_input.json'))
+    car, gas, trip = decode_json(jdata)
+    print("car: " + str(car))
+    print("gas " + str(gas))
+    print("trip " + str(trip))
+    return jsonify("fetch from a json file, check python terminal")
+
 
 @app.route('/', methods = ['GET'])
 def json_to_frontend():
