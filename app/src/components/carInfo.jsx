4f0d2d7f50e81cb43d42a27ec558c165d041f84d
xlei <xianmei_lei714@yahoo.com>
2018-10-31 14:58:31 -0700

    car info tab

---
+++ app/src/components/carInfo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class carInfo extends Component {
+	state = {};
+	render() {
+		return (
+			<div id="nav-car">
+				<p>car</p>
+			</div>
+		);
+	}
+}
+
+export default carInfo;
---
+++ app/src/components/dropDownList.jsx
@@ -0,0 +1,18 @@
+import React, { Component}  from 'react';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+
+const options = [
+    // ...
+    { value: 'Stanford University', label: 'Stanford' },
+    // ...
+];
+ 
+const field = ({ options }) => (
+    <Select
+        name="university"
+        value="one"
+        options={options}
+        onChange={val => console.log(val)}
+    />
+);
