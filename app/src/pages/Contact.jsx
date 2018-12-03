8f3346e11e0f5abf20e23ebb34afebaf551ba958
wilsenkosasih <aeroaeroga@yahoo.com>
2018-12-03 10:28:34 -0800

    contact form

--- app/src/pages/Contact.jsx
+++ app/src/pages/Contact.jsx
@@ -7,6 +7,9 @@ import img from "../img/gasMe.png";
 
 class Contact extends Component {
   state = {};
+  handleSubmit() {
+    alert('Sent')
+  }
   render() {
     return (
       <div>
@@ -14,32 +17,62 @@ class Contact extends Component {
           <Navbar />
           <Jumtotron title="Contact" subtitle="our contact" />
           <h2>Contact Us</h2>
-          <div className="row">
-          <div className="col-md-7 column">
-            Google Map Pic Here
-          </div>
-          <div className="col-md-5 column">
-            <img src={img} className="icon" alt="logo" />
-            <div className="text">
-              <div>
-                Phone: (xxx) xxx - xxxx
+          <div className="row flex">
+            <div className="col-md-1 flex">
+            </div>
+            <div className="col-md-3 form flex">
+              <form>
+                <div>
+                  <div>
+                    Name:
+                    </div>
+                  <div>
+                    <input type="text" name="name" style={{ width: "300px" }} />
+                  </div>
                 </div>
-              <div>
-                Fax: (xxx) xxx - xxxx
+                <div>
+                  <div>
+                    Email:
+                    </div>
+                  <div>
+                    <input type="text" name="email" style={{ width: "300px" }} />
+                  </div>
                 </div>
-              <div>
-                Email: xxxxxxxxxx@xxxxxx.xxx
+                <div>
+                  <div>
+                    Message:
+                  </div>
+                  <div>
+                    <textarea className="message" type="text" name="message" style={{ height: "175px", width: "300px" }} />
+                  </div>
                 </div>
-              <div>
-                Address: xxxxxxxxxxxxxxxx xxxxxxx xxxx, xxx
+                <button onClick={() => this.handleSubmit()}>Submit</button>
+              </form>
+            </div>
+            <div className="col-md-3 flex">
+            </div>
+            <div className="col-md-5 column flex location">
+              <img src={img} className="icon" alt="logo" />
+              <div className="text">
+                <div>
+                  Phone: (xxx) xxx - xxxx
                 </div>
+                <div>
+                  Fax: (xxx) xxx - xxxx
+                </div>
+                <div>
+                  Email: xxxxxxxxxx@xxxxxx.xxx
+                </div>
+                <div>
+                  Address: xxxxxxxxxxxxxxxx xxxxxxx xxxx, xxx
+                </div>
+              </div>
             </div>
           </div>
-          </div>
-          
+
         </div>
         <Footer />
-      </div>
+      </div >
     );
   }
 }
