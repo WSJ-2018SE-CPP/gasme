2a8189530df27b5b0c41205e1b84d4686de6298e
sammilei <xianmeilei@jpl.nasa.gov>
2018-12-03 22:47:42 -0800

    fix about page

--- app/src/pages/About.jsx
+++ app/src/pages/About.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Jumtotron from "../components/Jumbotron";
-//import PersonList from "../components/testing/PersonList"
-//import PersonInput from "../components/testing/PersonInput"
+import "./About.css";
 class About extends Component {
   state = {};
   render() {
@@ -11,15 +9,27 @@ class About extends Component {
       <div>
         <div className="container">
           <Navbar />
-          <Jumtotron title="About" subtitle="something about"/>
           <h2>About</h2>
-          <p>
-            think something, write something
-          </p>
+          <p>think something, write something</p>
         </div>
-        {/* <PersonList/>
-        <PersonInput/> */}
-        <Footer/>
+        <div className="jumbo">
+          <span className="jumbotron-text">
+            <h2 className="title">PLAN SMART, SAVE GAS COST.</h2>
+            <h5>
+              Wonder all the time where and when to fuel your car during daily
+              commute or long trips? Our trip planner can help you find the
+              cheapest route with over 150,000 gas stations in the U.S.
+            </h5>
+            <h4>
+              Plan your trip{" "}
+              <a href="http://www.gasmeonline.com/">
+                <span class="link">now</span>
+              </a>
+              !
+            </h4>
+          </span>
+        </div>
+        <Footer />
       </div>
     );
   }
